8e43974807f2b75ceee2ab39302fdc980ae10e16
Wes Hardaker <opensource@hardakers.net>
2024-06-04 20:12:50 -0700

    basic cleanup

--- dotnest/__init__.py
+++ dotnest/__init__.py
@@ -1,31 +1,35 @@
 """A simplistic text string access specification for deep structures."""
 
+from __future__ import annotations
+from typing import Any
+
 __VERSION__ = "0.9"
 
-class DotNest():
+class DotNest:
     """A class implementing data access by dotted-strings."""
-    def __init__(self, data):
+
+    def __init__(self, data: dict | list):
         """Initialize the class with an existing data structure."""
         self._data = data
 
     @property
-    def data(self):
+    def data(self) -> dict | list:
         """The encapsulated data."""
         return self._data
 
     @data.setter
-    def data(self, newdata):
+    def data(self, newdata: Any) -> None:
         self._data = newdata
 
-    def get(self, keys):
-        """given a list of keys, return the value at spot.
+    def get(self, keys: str | list) -> Any:
+        """Return the value at a spot given a list of keys.
 
-           keys must be either a dotted string ("element.other.3.foo")
-           or a list of keys already parsed (["element", "other", "3",
-           "foo"]).
+        keys must be either a dotted string ("element.other.3.foo")
+        or a list of keys already parsed (["element", "other", "3",
+        "foo"]).
 
-           keys will be used as either keys to access a dict or an
-           integer for accessing list elements.
+        keys will be used as either keys to access a dict or an
+        integer for accessing list elements.
         """
         keys = self.parse_keys(keys)
         ptr = self.data
@@ -42,38 +46,39 @@ class DotNest():
 
         return ptr
 
-    def set(self, keys, value):
-        "Given a key set (see get()), set the value at this spot to value."
+    def set(self, keys: str | list, value: Any) -> None:
+        """Given a key set (see get()), set the value at this spot to value."""
         keys = self.parse_keys(keys)
         ptr = self.get(keys[0:-1])
         ptr[keys[-1]] = value
 
-    def parse_keys(self, values):
+    def parse_keys(self, values: str | list) -> list:
         """Separate a list of keys by a '.' specifier.  AKA, split.
 
-        If values is already a list, this will simply return it as is."""
+        If values is already a list, this will simply return it as is.
+        """
         if isinstance(values, list):
             return values
-        # TODO: allow / pathing if values starts with a /?
-        # TODO: deal with escapes
+        # TODO(hardaker): allow / pathing if values starts with a /?
+        # TODO(hardaker): deal with escapes
         return values.split(".")
 
-    def __eq__(self, other):
+    def __eq__(self, other: DotNest) -> bool:
         """Report whether this instance's data is equal to anothers."""
         return self.deep_compare(self.data, other.data)
 
     # from https://stackoverflow.com/questions/25044073/comparing-python-objects-from-deepcopy
-    def deep_compare(self, left, right, excluded_keys = []):
+    def deep_compare(self, left, right, excluded_keys = []) -> bool:
         """Deeply compare two structures."""
         # TODO(hardaker): why can't we use an existing compare?  I don't remember.
         # convert left and right to dicts if possible, skip if they can't be converted
-        try: 
+        try:
             left = left.__dict__
             right = right.__dict__
         except Exception:
             pass
 
-        # both sides must be of the same type 
+        # both sides must be of the same type
         if type(left) != type(right):
             return False
 
@@ -85,16 +90,16 @@ class DotNest():
                     # check if the key is present in the right dict, if not, we are not equals
                     if key not in right:
                         return False
-                    else:
-                        # compare the values if the key is present in both sides
-                        if not self.deep_compare(left[key], right[key], excluded_keys):
-                            return False
+
+                    # compare the values if the key is present in both sides
+                    if not self.deep_compare(left[key], right[key], excluded_keys):
+                        return False
 
             # check if any keys are present in right, but not in left
             for key in right:
                 if key not in left and key not in excluded_keys:
                     return False
-            
+
             return True
 
         # check for each item in lists
